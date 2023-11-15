5659ca769e2de167aea705bb8ddad1dfd4f78513
Pryta Rosela <132034419+prytarosela@users.noreply.github.com>
2023-11-15 22:01:35 +0700

    Update Pengguna.java

--- Pengguna.java
+++ Pengguna.java
@@ -1,1 +1,22 @@
+public class Pengguna { // Kelas Pengguna untuk merepresentasikan pengguna dengan username dan password
+    // Variabel instance private untuk menyimpan username dan password
+    private String username;
+    private String password;
+
+    // Konstruktor untuk inisialisasi objek Pengguna
+    public Pengguna(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+     // Metode getter untuk mendapatkan username
+    public String getUsername() {
+        return username;
+    }
+
+    // Metode getter untuk mendapatkan password
+    public String getPassword() {
+        return password;
+    }
+}
 
